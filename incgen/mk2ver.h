a5961e63db6cf7a1cc270ae9e24ec82461b4240f
Abdulhadi5692HDI2-3 <abdulhadi5692hdi2@gmail.com>
2024-01-17 20:24:35 +0000

    make a iso with mkrelease.bat

--- incgen/mk2ver.h
+++ incgen/mk2ver.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "../external/mk2/MK2.Utils.h"
 #define BUILDNUM 1000
-#define BUILD_DATE_TIME "16/01/2024 AT 20:13:59.33"
-#define BUILD_DATE_TIMEW _WIDESTR("16/01/2024 AT 20:13:59.33")
+#define BUILD_DATE_TIME "17/01/2024 AT 19:58:08.07"
+#define BUILD_DATE_TIMEW _WIDESTR("17/01/2024 AT 19:58:08.07")
